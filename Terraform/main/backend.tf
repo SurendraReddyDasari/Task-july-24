3e9c3f0f724e98e50af39b18dbef9c276cbe86f1
DivyaMandati <divyamandati20@gmail.com>
2023-07-23 23:26:32 +0100

    modified

--- Terraform/main/backend.tf
+++ Terraform/main/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-remotestate31"
-    storage_account_name = "tfstateremote312"
+    storage_account_name = "tfstateremote31"
     container_name       = "remotestatetf31"
     key                  = "dev.terraform.tfstate"
   }
